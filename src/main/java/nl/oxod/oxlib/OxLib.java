989256a23db6de4a5054a50802c07ceac644d59c
oxod <git@oxod.nl>
2026-07-07 01:44:52 +0200

    feat: init

---
+++ src/main/java/nl/oxod/oxlib/OxLib.java
@@ -0,0 +1,10 @@
+package nl.oxod.oxlib;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class OxLib extends JavaPlugin implements Listener {
+  @Override
+  public void onEnable() {
+  }
+}
